d706efb29b738e5f1d528e3bd1ac5da11133faf0
lihui <me@lihui.net>
2022-07-31 10:49:14 +0800

    feat: renew index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: '源于实践',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+      Life wiki 的内容均来源于实践。
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '聚焦问题',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+      Life wiki 的内容聚焦在所遇到的问题上。
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: '运用实践解决问题，并更新实践',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        实践与问题相辅相成，从问题中总结实践，并在实践中发现问题。
       </>
     ),
   },
