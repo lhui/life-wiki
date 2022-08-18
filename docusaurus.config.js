78cabec66b4bcb8f7a838b5ac8901b9752b1820c
lihui <me@lihui.net>
2022-08-19 00:18:18 +0800

    feat: change edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/lhui/wiki/tree/master/docs/',
+            'https://github.com/lhui/wiki/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/lhui/wiki/tree/master/blog/',
+            'https://github.com/lhui/wiki/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
